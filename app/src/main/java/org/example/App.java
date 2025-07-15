4eec0017f1ea49241d8bedc8e0a48d227c74adf2
5ycm <brianthe4th@gmail.com>
2025-07-14 17:33:05 -0700

    Initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public String getGreeting(){
+        return "Hello!";
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        // Create your custom button(s)
+        ImageButton button1 = new ImageButton("/image.png");
+
+        // VBox aligned vertically, spacing between buttons
+        VBox vbox = new VBox(20); // 20px spacing
+        vbox.setAlignment(Pos.CENTER);   // Vertical center alignment
+        vbox.getChildren().addAll(button1);
+
+        // Put VBox on the right side of the screen using a BorderPane
+        BorderPane root = new BorderPane();
+        root.setRight(vbox); // VBox appears on the right
+
+        Scene scene = new Scene(root, 600, 400);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        primaryStage.setTitle("Right-Aligned VBox Layout");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ app/src/main/java/org/example/ImageButton.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ImageButton extends Button {
+    public ImageButton(String imagePath) {
+        if (imagePath == null) {
+            throw new IllegalArgumentException("imagePath was null in ImageButton Constructor");
+        }
+
+        Image icon = new Image(getClass().getResource(imagePath).toExternalForm());
+        ImageView imageView = new ImageView(icon);
+        imageView.setFitWidth(75);
+        imageView.setFitHeight(75);
+        imageView.setPreserveRatio(true);
+
+        this.setGraphic(imageView);
+        this.setPrefSize(100, 100);
+        this.getStyleClass().add("roundButton");
+    }
+}
